84041156d0f7bc39b1ee69d42797a739b41f8857
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-02-15 18:48:03 +0200

    Correct port to 3001

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/', (request, response) => {
         <p>${date}</p>`)
   })
   
-  const PORT = 3002
+  const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
