adaf0bd7ecf4eff90f531106cc0aa282fae537db
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-02-18 18:33:51 +0200

    Teht.3.11, fullstack

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 //app.use(morgan('tiny'))
 
 morgan.token('body', function getData (request){ //(request, response) => {
