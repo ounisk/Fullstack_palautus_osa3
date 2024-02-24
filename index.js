577da45facc26dbec4fe565bed5ffe3e10d0ec44
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-02-24 11:51:22 +0200

    Teht 3.15 ja 3.16, step3 ja step4

--- index.js
+++ index.js
@@ -9,22 +9,16 @@ const Person = require('./models/person')
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
-//app.use(morgan('tiny'))
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
-//const url =
-//  `mongodb+srv://fullstack:${password}@cluster0.5lrs0ck.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-//mongoose.set('strictQuery', false)
-//mongoose.connect(url)
-
-//const personSchema = new mongoose.Schema({
-//  name: String,
-//  number: String
-//})
-
-//const Person = mongoose.model('Person', personSchema)
-
+app.use(requestLogger)
 
 
 morgan.token('body', function getData (request){ //(request, response) => {
@@ -34,27 +28,7 @@ morgan.token('body', function getData (request){ //(request, response) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
 
 
-//let persons =[
-//    { 
-//        id:1,
-//        name: 'Arto Hellas',
-//        number: '040-123456'
-//    },
-//    { 
-//        id:2,
-//        name: 'Ada Lovelace',
-//        number: '39-44-5323523' 
-//    },
-//    { 
-//        id:3,
-//        name: 'Dan Abramov',
-//        number: '12-43-234345'
-//    },
-//    {
-//        id:4,
-//        name: 'Mary Poppendieck', 
-//        number: '39-23-6423122' }
-//]
+
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -84,17 +58,29 @@ app.get('/', (request, response) => {
     //} else {
     //    response.status(404).end() 
     //}
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
-    })
+    Person.findById(request.params.id)
+      .then(person => {
+        if (person) {
+          response.json(person)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+        //{console.log(error)
+        //response.status(400).send({error:"malformatted id"})
+      //  })
   })
   
-//  app.delete('/api/persons/:id', (request, response) => {
-//    const id = Number(request.params.id)
-//    persons = persons.filter(person => person.id !== id)
-//  
-//    response.status(204).end()
-//  })
+  app.delete('/api/persons/:id', (request, response, next) => {
+    //const id = Number(request.params.id)
+    //persons = persons.filter(person => person.id !== id)
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error =>next(error))
+  })
 
 
   const generateId = () => {
@@ -112,7 +98,7 @@ app.get('/', (request, response) => {
     return persId
   }
   
-  app.post('/api/persons', (request, response) => {     // MUUTA tämä 3.14
+  app.post('/api/persons', (request, response) => {     // TEHTY 3.14
     const body = request.body
     console.log('nimi, numero, id', body.name, body.number)
     if (!body.name) {
@@ -146,6 +132,28 @@ app.get('/', (request, response) => {
       response.json(savedPerson)
     })
   })
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  // olemattomien osoitteiden käsittely
+  app.use(unknownEndpoint)
+
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+  }
+  
+  // tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
+  app.use(errorHandler)
+
 
 
   const PORT = process.env.PORT
