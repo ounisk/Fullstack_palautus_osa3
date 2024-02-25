8cb3bad9e1d5018fe35d2d90b2bdb830055517bc
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2024-02-25 13:43:19 +0200

    Teht. 3.20, step8

--- models/person.js
+++ models/person.js
@@ -18,7 +18,17 @@ const personSchema = new mongoose.Schema({
         minlength: 3,
         required: true
     },
-    number: String
+    number:{
+        type: String,
+        minlength: 8,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^(\d{2}-\d{6,}|\d{3}-\d{5,})$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    }
     })
 
 personSchema.set('toJSON', {
